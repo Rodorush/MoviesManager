75a088e1b80e4ccbe0349fe6aef43803e7ece830
Rodorush <rodolfop.andrade@gmail.com>
2023-12-10 16:41:50 -0300

    Toolbar na MainActivity e tema
    • Criado Toolbar na MainActivity
    • Alterado tema de Material para Material3, pois anterior não estava compilando.

--- app/src/main/java/br/com/rodorush/moviesmanager/view/MainActivity.kt
+++ app/src/main/java/br/com/rodorush/moviesmanager/view/MainActivity.kt
@@ -1,10 +1,12 @@
-package br.com.rodorush.moviesmanager
+package br.com.rodorush.moviesmanager.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rodorush.moviesmanager.R
 import br.com.rodorush.moviesmanager.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -12,6 +14,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        setSupportActionBar(amb.mainTb)
+        supportActionBar?.title = getString(R.string.app_name)
 
     }
 }
